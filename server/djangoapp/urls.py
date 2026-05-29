225bd39a4546eef656ad4682d9ddf5d7f981fa6a
Shaikhah <shaalsh9@gmail.com>
2026-05-30 02:21:41 +0300

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,11 +4,39 @@ from . import views
 app_name = 'djangoapp'
 
 urlpatterns = [
-    path('get_dealers', views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
-    path('get_cars', views.get_cars, name='get_cars'),
-    path('login', views.login, name='login'),
-    path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
-    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_reviews'),
-    path('add_review', views.add_review, name='add_review'),
+    path(
+        'get_dealers',
+        views.get_dealerships,
+        name='get_dealers'
+    ),
+    path(
+        'get_dealers/<str:state>',
+        views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        'get_cars',
+        views.get_cars,
+        name='get_cars'
+    ),
+    path(
+        'login',
+        views.login,
+        name='login'
+    ),
+    path(
+        'dealer/<int:dealer_id>',
+        views.get_dealer_details,
+        name='dealer_details'
+    ),
+    path(
+        'reviews/dealer/<int:dealer_id>',
+        views.get_dealer_reviews,
+        name='dealer_reviews'
+    ),
+    path(
+        'add_review',
+        views.add_review,
+        name='add_review'
+    ),
 ]
